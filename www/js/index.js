413a4d86f473d3717b7f953a6bf58c6491deeb8f
paulmboyce <paul.boyce@goobito.com>
2020-04-11 10:05:35 -0300

    Added Geolocation demo code. Updated .gitignore

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,60 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+
+        console.log("navigator.geolocation plugin is active when 'deviceready' fires.");        
         this.receivedEvent('deviceready');
+
+
+        // onSuccess Callback
+        // This method accepts a Position object, which contains the
+        // current GPS coordinates
+        //
+        var onSuccess = function(position) {
+
+                var element = document.getElementById('geolocation');
+                element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                                    'Longitude: ' + position.coords.longitude     + '<br />' +
+                                    '<hr />'      + element.innerHTML;
+
+            alert('Latitude: '          + position.coords.latitude          + '\n' +
+                'Longitude: '         + position.coords.longitude         + '\n' +
+                'Accuracy: '          + position.coords.accuracy          + '\n' +
+                'Timestamp: '         + position.timestamp                + '\n');
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }
+
+        let currentPositionOptions = { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true };    
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, currentPositionOptions);
+
+
+        //   This method accepts a `Position` object, which contains
+        //   the current GPS coordinates
+        //
+        function onWatchSuccess(position) {
+            var element = document.getElementById('geolocation');
+            element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                                'Longitude: ' + position.coords.longitude     + '<br />' +
+                                '<hr />';
+        }
+
+        // onError Callback receives a PositionError object
+        //
+        function onWatchError(error) {
+            alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }
+
+        // Options: throw an error if no update is received every 30 seconds.
+        //
+        var watchID = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, { timeout: 30000 });
+
     },
 
     // Update DOM on a Received Event
